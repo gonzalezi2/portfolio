706af8795f2d0820e6f2ca98821e779343eb857d
Ivan <gonzalezim02@gmail.com>
2018-11-09 18:03:20 -0500

    Created new styles for the dashboard

--- js/script.js
+++ js/script.js
@@ -7,10 +7,14 @@
       var lat = position.coords.latitude;
       var long = position.coords.longitude;
       var appid = '1ea9e9eba28e8ef98d526583b03739ea';
-      fetch("https://api.openweathermap.org/data/2.5/weather?lat=".concat(lat, "&lon=").concat(long, "&APPID=").concat(appid)).then(function (res) {
+      var units = ['imperial', 'metric'];
+      fetch("https://api.openweathermap.org/data/2.5/weather?lat=".concat(lat, "&lon=").concat(long, "&APPID=").concat(appid, "&units=").concat(units[0])).then(function (res) {
         return res.json();
       }).then(function (response) {
         console.log(response);
+        document.querySelector('#city').innerHTML = response.name;
+        document.querySelector('#temp').innerHTML = "".concat(response.main.temp, " &deg;");
+        document.querySelector('#weather').innerHTML = response.weather[0].main;
       });
     });
   }
--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@
             let lat = position.coords.latitude;
             let long = position.coords.longitude;
             let appid = '1ea9e9eba28e8ef98d526583b03739ea';
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${appid}`)
+            let units = ['imperial', 'metric'];
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${appid}&units=${units[0]}`)
                 .then(res => {
                     return res.json();
                 })
                 .then(response => {
-                    console.log(response);
+                    document.querySelector('#city').innerHTML = response.name;
+                    document.querySelector('#temp').innerHTML = `${response.main.temp} &deg;`;
+                    document.querySelector('#weather').innerHTML = response.weather[0].main;
                 });
         });
     }
